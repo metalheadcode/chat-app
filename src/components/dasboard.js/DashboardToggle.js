fef98009a74b23a561c09fca617f7b6f9cfbcd45
Ahmad Hazim <metalheadcoder@gmail.com>
2021-08-06 23:35:12 +0800

    add respinsive sidebar with custom hook

--- src/components/dasboard.js/DashboardToggle.js
+++ src/components/dasboard.js/DashboardToggle.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Button, Drawer, Icon } from 'rsuite';
 import Dashboard from '.';
-import { useModalState } from '../../misc/custom-hooks';
+import { useMediaqQueryChrome, useModalState } from '../../misc/custom-hooks';
 
 const DashboardToggle = () => {
   const { isOpen, open, close } = useModalState();
+  const isMobile = useMediaqQueryChrome('(max-width: 992px)');
+
+  console.log(isMobile);
   return (
     <>
       <Button block color="blue" onClick={open}>
         <Icon icon="dashboard" /> Dashboard
       </Button>
-      <Drawer show={isOpen} onHide={close} placement="left">
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard />
       </Drawer>
     </>
--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export function useModalState(defaultValue = false) {
   const [isOpen, setIsOpen] = useState(defaultValue);
@@ -7,4 +7,42 @@ export function useModalState(defaultValue = false) {
   const close = () => setIsOpen(false);
 
   return { isOpen, open, close };
+}
+
+export function useMediaqQuerySafari(query) {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const queryList = window.matchMedia(query);
+    setMatches(queryList.matches);
+
+    queryList.addEventListener('change', event => setMatches(event.matches));
+
+    return () =>
+      queryList.removeEventListener('change', event =>
+        setMatches(event.matches)
+      );
+  }, [query]);
+
+  return matches;
+}
+
+export function useMediaqQueryChrome(query) {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const queryList = window.matchMedia(query);
+    setMatches(queryList.matches);
+
+    const listener = event => setMatches(event.matches);
+
+    queryList.addListener(listener);
+    return () => queryList.removeListener(listener);
+  }, [query]);
+
+  return matches;
 }
