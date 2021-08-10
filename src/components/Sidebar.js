6088c3c3d987f3f1fecfbecb590a9538f1ebb453
Ahmad Hazim <metalheadcoder@gmail.com>
2021-08-10 21:05:02 +0800

    add useEffect everytime topSideBarRef change, the height will be changes

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Divider } from 'rsuite';
 import CreateRoomBtnModal from './CreateRoomBtnModal';
 import DashboardToggle from './dasboard.js/DashboardToggle';
+import ChatRoomList from './rooms/ChatRoomList';
 
 const Sidebar = () => {
+  const topSideBarRef = useRef();
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (topSideBarRef.current) {
+      setHeight(topSideBarRef.current.scrollHeight);
+    }
+  }, [topSideBarRef]);
+
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSideBarRef}>
         <DashboardToggle />
         <CreateRoomBtnModal />
+        <Divider>Join Conversation</Divider>
       </div>
-      bottom
+      <ChatRoomList aboveElHeight={height} />
     </div>
   );
 };
