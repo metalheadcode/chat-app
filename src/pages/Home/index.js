656ee372feaeaa2c8662156fb41c59401799eee7
Ahmad Hazim <metalheadcoder@gmail.com>
2021-08-12 12:56:37 +0800

    change the path of Home, and change from home to index

---
+++ src/pages/Home/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useRouteMatch } from 'react-router';
+import { Switch, Route } from 'react-router-dom';
+import { Grid, Row, Col } from 'rsuite';
+import Sidebar from '../../components/Sidebar';
+import { RoomsProvider } from '../../context/rooms.context';
+import { useMediaqQueryChrome } from '../../misc/custom-hooks';
+import Chat from './Chat';
+
+const Home = () => {
+  // return boolean
+  const isDesktop = useMediaqQueryChrome('(min-width:992px');
+  console.log('isDesktop', isDesktop);
+  // return boolean
+  const { isExact } = useRouteMatch('/');
+  console.log('isExact', isExact);
+  // switch statement
+  const switchRenderSidebar = isDesktop || isExact;
+  console.log('switchRenderSidebar', switchRenderSidebar);
+
+  return (
+    <RoomsProvider>
+      <Grid fluid className="h-100">
+        <Row className="h-100">
+          {switchRenderSidebar && (
+            <Col xs={24} md={8} className="h-100">
+              <Sidebar />
+            </Col>
+          )}
+          <Switch>
+            <Route exact path="/chat/:chatId">
+              <Col xs={24} md={16} className="h-100">
+                <Chat />
+              </Col>
+            </Route>
+            <Route>
+              {isDesktop && (
+                <Col xs={24} md={16} className="h-100">
+                  <h6 className="text-center mt-page">Please select chat</h6>
+                </Col>
+              )}
+            </Route>
+          </Switch>
+        </Row>
+      </Grid>
+    </RoomsProvider>
+  );
+};
+
+export default Home;
