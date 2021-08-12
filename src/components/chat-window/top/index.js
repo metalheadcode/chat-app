67a3a534884844e5e5f54b16484a8b10b77f2dec
Ahmad Hazim <metalheadcoder@gmail.com>
2021-08-12 18:42:55 +0800

    enhanced Chat Top component

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar, Icon } from 'rsuite';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { useMediaqQueryChrome } from '../../../misc/custom-hooks';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
-const ChatTop = ({ name, description }) => {
+const ChatTop = () => {
+  const name = useCurrentRoom(value => value.name);
+  const isMobile = useMediaqQueryChrome('(max-width: 992px)');
+
   return (
     <div>
-      <h4>{name}</h4>
-      <p>{description}</p>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Icon
+            componentClass={Link}
+            to="/"
+            icon="arrow-circle-left"
+            size="2x"
+            className={
+              isMobile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-none'
+            }
+          />
+          <span className="text-disappear">{name}</span>
+        </h4>
+        <ButtonToolbar className="ws-nowrap">todo</ButtonToolbar>
+      </div>
+
+      <div className="d-flex justify-content-between align-items-center">
+        <span>todo</span>
+        <RoomInfoBtnModal />
+      </div>
     </div>
   );
 };
 
-export default ChatTop;
+export default React.memo(ChatTop);
