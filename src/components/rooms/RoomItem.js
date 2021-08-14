7e1561dff82134acf3bbe2d46acc4c168947d26e
Ahmad Hazim <metalheadcoder@gmail.com>
2021-08-14 12:51:20 +0800

    update RoomItem component align with rooms context

--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
+import ProfileAvatar from '../dasboard.js/ProfileAvatar';
 
 const RoomItem = ({ room }) => {
-  const { createdAt, name } = room;
+  console.log('last message obj', room);
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
-        <h3 className="text-diseapears">{name}</h3>
-        <TimeAgo
-          datetime={new Date(createdAt)}
-          className="font-normal text-black-45"
-        />
+        <h3 className="text-disappear">{room.name}</h3>
+        {room.lastMessage ? (
+          <TimeAgo
+            datetime={new Date(room.lastMessage.createdAt)}
+            className="font-normal text-black-45"
+          />
+        ) : (
+          <TimeAgo
+            datetime={new Date(room.createdAt)}
+            className="font-normal text-black-45"
+          />
+        )}
       </div>
       <div className="d-flex align-items-center text-black-70">
-        <span>No messages yet....</span>
+        {room.lastMessage ? (
+          <>
+            <div className="d-flex align-items-center">
+              <ProfileAvatar
+                src={room.lastMessage.author.avatar}
+                name={room.lastMessage.author.name}
+                size="sm"
+              />
+            </div>
+            <div className="text-disappear ml-2">
+              <div className="italic">{room.lastMessage.author.name}</div>
+              <span>{room.lastMessage.text}</span>
+            </div>
+          </>
+        ) : (
+          <span>No message yet</span>
+        )}
       </div>
     </div>
   );
