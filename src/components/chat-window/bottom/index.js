dd2574069a3a8f6b9c493e6c208c2a5e87e7ee20
Ahmad Hazim <metalheadcoder@gmail.com>
2021-08-14 11:55:59 +0800

    edit ChatBottom

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -1,37 +1,40 @@
 import React, { useState, useCallback } from 'react';
 import firebase from 'firebase/app';
-import { Icon, Input, InputGroup } from 'rsuite';
+import { Alert, Icon, Input, InputGroup } from 'rsuite';
+import { useParams } from 'react-router-dom';
 import { useProfile } from '../../../context/profile.context';
 import { database } from '../../../misc/firebase';
-import { useRooms } from '../../../context/rooms.context';
-import { useParams } from 'react-router-dom';
 
 const ChatBottom = () => {
   const [input, setInput] = useState('');
   const { profile } = useProfile();
   const { chatId } = useParams();
-  const rooms = useRooms();
 
   const onChangeInput = useCallback(value => {
     setInput(value);
   }, []);
 
-  const onSendClick = () => {
+  const onSendClick = async () => {
     if (input) {
       const chatRef = database.ref('messages');
       const messageId = chatRef.push().key;
       const newChat = {
-        roomId: chatId,
         author: {
+          ...(profile.avatar ? { avatar: profile.avatar } : {}),
+          createdAt: profile.createdAt,
           name: profile.name,
           uid: profile.uid,
-          createdAt: profile.createdAt,
-          ...(profile.avatar ? { avatar: profile.avatar } : {}),
         },
         createdAt: firebase.database.ServerValue.TIMESTAMP,
+        roomId: chatId,
         text: input,
       };
-      chatRef.child(chatKey).update(newChat);
+
+      try {
+        await chatRef.child(messageId).update(newChat);
+      } catch (error) {
+        Alert.error(error.message, 4000);
+      }
     }
   };
 
