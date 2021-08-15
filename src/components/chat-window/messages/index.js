b945a88bde16254e6bb30885ade17a15dd295ebf
metalheadcode <metalheadcoder@gmail.com>
2021-08-15 20:29:34 +0800

    create Message container for messgeitem

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { database } from '../../../misc/firebase';
+import { transformToArrayWithId } from '../../../misc/helpers';
+import MessageItem from './MessageItem';
 
 const Messages = () => {
-  return <div>Messages</div>;
+  const { chatId } = useParams();
+  const [messages, setMessages] = useState(null);
+
+  const isChatEmpty = messages && messages.length === 0;
+  const isShowMessage = messages && messages.length > 0;
+
+  useEffect(() => {
+    const messageRef = database
+      .ref('messages')
+      .orderByChild('roomId')
+      .equalTo(chatId);
+
+    messageRef.on('value', snap => {
+      const data = transformToArrayWithId(snap.val());
+      setMessages(data);
+    });
+
+    return () => messageRef.off('value');
+  }, [chatId]);
+
+  console.log('Message Item', messages);
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>No message</li>}
+      {isShowMessage &&
+        messages.map(message => (
+          <MessageItem key={message.id} message={message} />
+        ))}
+    </ul>
+  );
 };
 
 export default Messages;
