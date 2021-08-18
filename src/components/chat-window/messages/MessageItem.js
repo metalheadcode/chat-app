78a1f5f69a1e76c6eeb660029088d96ab585432c
metalheadcode <metalheadcoder@gmail.com>
2021-08-18 18:05:30 +0800

    apply custom hook inside

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
+import { useHover } from '../../../misc/custom-hooks';
 import ProfileAvatar from '../../dasboard.js/ProfileAvatar';
 import PresenceDot from '../../PresenceDot';
+import IconBtnControl from './IconBtnControl';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 const MessageItem = ({ message }) => {
   const { author, createdAt, text } = message;
+  const [selfRef, isHover] = useHover();
+
   return (
-    <li className="padded mb-1">
+    <li
+      className={`padded mb-1 cursor-pointer ${isHover ? 'bg-black-02' : ''}`}
+      ref={selfRef}
+    >
       <div className="d-flex align-items-center font-bolder mb-1">
         <PresenceDot uid={author.uid} />
         <ProfileAvatar
@@ -24,6 +31,13 @@ const MessageItem = ({ message }) => {
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
+        />
+        <IconBtnControl
+          isVisible
+          iconName="heart"
+          tooltip="Like this message"
+          onClick
+          badgeContent={5}
         />
       </div>
       <div>
