2e4b3b1bcd4c5594fc27c7684621c0463ab1f531
metalheadcode <metalheadcoder@gmail.com>
2021-08-23 22:50:59 +0800

    create visibility button logic for admin, so here im using currentRoom context api

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
+import { useCurrentRoom } from '../../../context/current-room.context';
 import { useHover, useMediaqQueryChrome } from '../../../misc/custom-hooks';
+import { auth } from '../../../misc/firebase';
 import ProfileAvatar from '../../dasboard.js/ProfileAvatar';
 import PresenceDot from '../../PresenceDot';
 import IconBtnControl from './IconBtnControl';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
-const MessageItem = ({ message, handleLike, handleDelete }) => {
+const MessageItem = ({ message, handleLike, handleDelete, handleAdmin }) => {
   const { author, createdAt, text, likes, likeCount } = message;
   const [selfRef, isHover] = useHover();
-  const isAuthor = true;
   const isMobile = useMediaqQueryChrome('(max-width: 992px)');
   const canShowIcon = isMobile || isHover;
+  const isAdmin = useCurrentRoom(value => value.isAdmin);
+  const admin = useCurrentRoom(value => value.admin);
+
+  const isMsgAuthorAdmin = admin.includes(author.uid);
+  const isAuthor = auth.currentUser.uid === author.uid;
+  const canGrantAdmin = isAdmin && !isAuthor;
 
   return (
     <li
@@ -30,7 +38,15 @@ const MessageItem = ({ message, handleLike, handleDelete }) => {
           author={author}
           appearance="link"
           className="p-0 ml-1 text-black"
-        />
+        >
+          {canGrantAdmin && (
+            <Button block onClick={() => handleAdmin(author.uid)} color="blue">
+              {isMsgAuthorAdmin
+                ? 'Remove Admin Permision'
+                : 'Give admin in this role'}
+            </Button>
+          )}
+        </ProfileInfoBtnModal>
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
@@ -60,4 +76,4 @@ const MessageItem = ({ message, handleLike, handleDelete }) => {
   );
 };
 
-export default MessageItem;
+export default React.memo(MessageItem);
