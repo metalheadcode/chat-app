edcef2e6625cc2309ee162d969c0288790656388
metalheadcode <metalheadcoder@gmail.com>
2021-08-24 23:27:40 +0800

    create handleDelete

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -80,6 +80,41 @@ const Messages = () => {
     Alert.info(alertMsg, 4000);
   }, []);
 
+  const handleDelete = useCallback(
+    async msgId => {
+      if (!window.confirm('Delete this message?')) {
+        return;
+      }
+
+      const isLast = messages[messages.length - 1].id === msgId;
+
+      const updates = {};
+
+      updates[`/messages/${msgId}`] = null;
+
+      // if the message was the last message deleted
+      if (isLast && messages.length > 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = {
+          ...messages[messages.length - 2],
+          msgId: messages[messages.length - 2].id,
+        };
+      }
+
+      // delete last message
+      if (isLast && messages.length === 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = null;
+      }
+
+      try {
+        await database.ref().update(updates);
+        Alert.info('Messages has been deleted', 4000);
+      } catch (error) {
+        Alert.error(error.message, 4000);
+      }
+    },
+    [chatId, messages]
+  );
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No message</li>}
@@ -89,6 +124,7 @@ const Messages = () => {
             key={message.id}
             message={message}
             handleLike={handleLike}
+            handleDelete={handleDelete}
             handleAdmin={handleAdmin}
           />
         ))}
